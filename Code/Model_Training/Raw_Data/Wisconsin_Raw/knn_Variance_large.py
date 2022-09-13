b323217ec65054927115ced6ff4e9ce6009f0a85
Stephen P <stepheneprice1@gmail.com>
2022-09-13 09:24:08 -0700

    Ran 100 data augmentation trials

--- Code/Model_Training/Raw_Data/Wisconsin_Raw/knn_Variance_large.py
+++ Code/Model_Training/Raw_Data/Wisconsin_Raw/knn_Variance_large.py
@@ -16,35 +16,64 @@ from sklearn.metrics import confusion_matrix
 import warnings #to remove the warnings
 warnings.filterwarnings('ignore')
 
+def generateNewFeatureValMultiple(df, col, numberOfBins, numPatients):
+    hist, bins = np.histogram(df[col], bins=numberOfBins)
+    bin_midpoints = bins[:-1] + np.diff(bins)/2
+    cdf = np.cumsum(hist)
+    cdf = cdf / cdf[-1]
+    values = np.random.rand(numPatients)
+    value_bins = np.searchsorted(cdf, values)
+    random_from_cdf = bin_midpoints[value_bins]
+    return list(random_from_cdf)
+
 
 cols = ['ID', 'Diagnosis', 'radius', 'texture', 'perimeter', 'area', 'smoothness', 'compactness', 'concavity', 'concave_points', 'symmetry', 'fractal_dimension',
         'radius_SE', 'texture_SE', 'perimeter_SE', 'area_SE', 'smoothness_SE', 'compactness_SE', 'concavity_SE', 'concave_points_SE', 'symmetry_SE', 'fractal_dimension_SE',
         'radius_MAX', 'texture_MAX', 'perimeter_MAX', 'area_MAX', 'smoothness_MAX', 'compactness_MAX', 'concavity_MAX', 'concave_points_MAX', 'symmetry_MAX', 'fractal_dimension_MAX']
 df = pd.read_csv('../../../../Wisconsin_Database/wdbc.data', header=None)
 
-#df = pd.read_csv('../../../../Wisconsin_Database/augmented_wbdc_20000.data')
-#cols = ['Diagnosis', 'radius', 'texture', 'perimeter', 'area', 'smoothness', 'compactness', 'concavity', 'concave_points', 'symmetry', 'fractal_dimension',
-#        'radius_SE', 'texture_SE', 'perimeter_SE', 'area_SE', 'smoothness_SE', 'compactness_SE', 'concavity_SE', 'concave_points_SE', 'symmetry_SE', 'fractal_dimension_SE',
-#        'radius_MAX', 'texture_MAX', 'perimeter_MAX', 'area_MAX', 'smoothness_MAX', 'compactness_MAX', 'concavity_MAX', 'concave_points_MAX', 'symmetry_MAX', 'fractal_dimension_MAX']
-#df.columns = cols
-
-
+'''
+df = pd.read_csv('../../../../Wisconsin_Database/augmented_wbdc_20000.data')
+cols = ['Diagnosis', 'radius', 'texture', 'perimeter', 'area', 'smoothness', 'compactness', 'concavity', 'concave_points', 'symmetry', 'fractal_dimension',
+        'radius_SE', 'texture_SE', 'perimeter_SE', 'area_SE', 'smoothness_SE', 'compactness_SE', 'concavity_SE', 'concave_points_SE', 'symmetry_SE', 'fractal_dimension_SE',
+        'radius_MAX', 'texture_MAX', 'perimeter_MAX', 'area_MAX', 'smoothness_MAX', 'compactness_MAX', 'concavity_MAX', 'concave_points_MAX', 'symmetry_MAX', 'fractal_dimension_MAX']
+'''
 df.columns = cols
 df['Diagnosis'] = df['Diagnosis'].map(
                    {'M':True,'B':False})
-print(df.shape)
-
+df = df[df.columns[1:]]
 knnScores = []
 naiveBaysScores = []
 logisticRegressionScores = []
 
 for i in range(100):
     X = df[cols[2:]]
-    #X = df[cols[1:]]
     y = df[cols[1]]
 
     SEED = 42 + i
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.95, random_state=SEED)
+    
+    trainingSet = X_train.copy()
+    trainingSet['Diagnosis'] = y_train
+    arr2D = trainingSet.to_numpy()
+    columnIndex = 30
+    sortedArr = arr2D[arr2D[:,columnIndex].argsort()]
+    numZeros = list(sortedArr[:, columnIndex]).count(0)
+    benignDF, malignantDF = pd.DataFrame(sortedArr[0:numZeros]), pd.DataFrame(sortedArr[numZeros:])
+    benignDF.columns = trainingSet.columns
+    malignantDF.columns = trainingSet.columns
+    benignPatients = []
+    for col in benignDF.columns:
+        benignPatients.append(generateNewFeatureValMultiple(benignDF, col, 25, 10000))
+    beningPatients = np.array(benignPatients).T
+    malignangPatients = []
+    for col in malignantDF.columns:
+        malignangPatients.append(generateNewFeatureValMultiple(malignantDF, col, 25, 10000))
+    malignangPatients = np.array(malignangPatients).T
+    jointArray = np.vstack((beningPatients,malignangPatients))
+    finalDataset = pd.DataFrame(jointArray, columns = benignDF.columns)
+    X_train = finalDataset[finalDataset.columns[:-1]]
+    y_train = finalDataset[finalDataset.columns[-1]]
     
     scaler = StandardScaler()
     scaler.fit(X_train)
@@ -53,17 +82,23 @@ for i in range(100):
     scaled_df = pd.DataFrame(X_train, columns=col_names)
     scaled_df.describe().T
     
+
+
     ''' KNN '''
 
+    
     error = []
     score_vals = []
-    for j in range(1, min(len(y_test), len(y_train)) - 1):
+    for j in range(1, 5000, 75):
         knn = KNeighborsRegressor(n_neighbors=j)
         knn.fit(X_train, y_train)
         pred_i = knn.predict(X_test).round(decimals=0)
         mae = mean_absolute_error(y_test, pred_i)
         error.append(mae)
         score_vals.append(knn.score(X_test, y_test))
+        y_pred = knn.predict(X_test).round(decimals=0)
+        y_pred = y_pred == 1
+        performanceAccuracy = sum(y_pred == y_test)/len(y_test)
     bestIndex = score_vals.index(max(score_vals)) + 1  #Adds one because it starts with 1 neighbor not 0
 
     regressor = KNeighborsRegressor(n_neighbors=bestIndex)
@@ -73,6 +108,7 @@ for i in range(100):
     #print('Iteration {}: '.format(i))
     performanceAccuracy = sum(y_pred == y_test)/len(y_test)
     knnScores.append(performanceAccuracy)
+    
 
     '''Naive Bayes'''
 
--- Code/Model_Training/Raw_Data/Wisconsin_Raw/knn_Variance_small_LEAKAGE.py
+++ Code/Model_Training/Raw_Data/Wisconsin_Raw/knn_Variance_small_LEAKAGE.py
@@ -17,35 +17,28 @@ import warnings #to remove the warnings
 warnings.filterwarnings('ignore')
 
 
-cols = ['ID', 'Diagnosis', 'radius', 'texture', 'perimeter', 'area', 'smoothness', 'compactness', 'concavity', 'concave_points', 'symmetry', 'fractal_dimension',
-        'radius_SE', 'texture_SE', 'perimeter_SE', 'area_SE', 'smoothness_SE', 'compactness_SE', 'concavity_SE', 'concave_points_SE', 'symmetry_SE', 'fractal_dimension_SE',
-        'radius_MAX', 'texture_MAX', 'perimeter_MAX', 'area_MAX', 'smoothness_MAX', 'compactness_MAX', 'concavity_MAX', 'concave_points_MAX', 'symmetry_MAX', 'fractal_dimension_MAX']
-df = pd.read_csv('../../../../Wisconsin_Database/wdbc.data', header=None)
-
-#df = pd.read_csv('../../../../Wisconsin_Database/augmented_wbdc_20000.data')
-#cols = ['Diagnosis', 'radius', 'texture', 'perimeter', 'area', 'smoothness', 'compactness', 'concavity', 'concave_points', 'symmetry', 'fractal_dimension',
+#cols = ['ID', 'Diagnosis', 'radius', 'texture', 'perimeter', 'area', 'smoothness', 'compactness', 'concavity', 'concave_points', 'symmetry', 'fractal_dimension',
 #        'radius_SE', 'texture_SE', 'perimeter_SE', 'area_SE', 'smoothness_SE', 'compactness_SE', 'concavity_SE', 'concave_points_SE', 'symmetry_SE', 'fractal_dimension_SE',
 #        'radius_MAX', 'texture_MAX', 'perimeter_MAX', 'area_MAX', 'smoothness_MAX', 'compactness_MAX', 'concavity_MAX', 'concave_points_MAX', 'symmetry_MAX', 'fractal_dimension_MAX']
-#df.columns = cols
+#df = pd.read_csv('../../../../Wisconsin_Database/wdbc.data', header=None)
 
+df = pd.read_csv('../../../../Wisconsin_Database/augmented_wbdc_20000.data')
+cols = ['Diagnosis', 'radius', 'texture', 'perimeter', 'area', 'smoothness', 'compactness', 'concavity', 'concave_points', 'symmetry', 'fractal_dimension',
+        'radius_SE', 'texture_SE', 'perimeter_SE', 'area_SE', 'smoothness_SE', 'compactness_SE', 'concavity_SE', 'concave_points_SE', 'symmetry_SE', 'fractal_dimension_SE',
+        'radius_MAX', 'texture_MAX', 'perimeter_MAX', 'area_MAX', 'smoothness_MAX', 'compactness_MAX', 'concavity_MAX', 'concave_points_MAX', 'symmetry_MAX', 'fractal_dimension_MAX']
 
 df.columns = cols
-df['Diagnosis'] = df['Diagnosis'].map(
-                   {'M':True,'B':False})
-print(df.shape)
-
 knnScores = []
 naiveBaysScores = []
 logisticRegressionScores = []
 
 for i in range(100):
-    X = df[cols[2:]]
-    #X = df[cols[1:]]
-    y = df[cols[1]]
+    #X = df[cols[2:]]
+    X = df[cols[1:]]
+    y = df[cols[0]]
 
     SEED = 42 + i
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.95, random_state=SEED)
-    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=SEED)
     scaler = StandardScaler()
     scaler.fit(X_train)
 
@@ -55,15 +48,21 @@ for i in range(100):
     
     ''' KNN '''
 
+    '''
     error = []
     score_vals = []
-    for j in range(1, min(len(y_test), len(y_train)) - 1):
+    for j in range(100, 150):
         knn = KNeighborsRegressor(n_neighbors=j)
         knn.fit(X_train, y_train)
         pred_i = knn.predict(X_test).round(decimals=0)
         mae = mean_absolute_error(y_test, pred_i)
         error.append(mae)
         score_vals.append(knn.score(X_test, y_test))
+        print('I, J: {}'.format(j))
+        y_pred = knn.predict(X_test).round(decimals=0)
+        y_pred = y_pred == 1
+        performanceAccuracy = sum(y_pred == y_test)/len(y_test)
+        print(performanceAccuracy)
     bestIndex = score_vals.index(max(score_vals)) + 1  #Adds one because it starts with 1 neighbor not 0
 
     regressor = KNeighborsRegressor(n_neighbors=bestIndex)
@@ -73,6 +72,7 @@ for i in range(100):
     #print('Iteration {}: '.format(i))
     performanceAccuracy = sum(y_pred == y_test)/len(y_test)
     knnScores.append(performanceAccuracy)
+    '''
 
     '''Naive Bayes'''
 
@@ -92,6 +92,6 @@ for i in range(100):
     print('Iteration: {}'.format(i))
 
 
-print('Min: {}, Max: {}, Avg: {}'.format(min(knnScores), max(knnScores), sum(knnScores)/len(knnScores)))
+#print('Min: {}, Max: {}, Avg: {}'.format(min(knnScores), max(knnScores), sum(knnScores)/len(knnScores)))
 print('Min: {}, Max: {}, Avg: {}'.format(min(naiveBaysScores), max(naiveBaysScores), sum(naiveBaysScores)/len(naiveBaysScores)))
 print('Min: {}, Max: {}, Avg: {}'.format(min(logisticRegressionScores), max(logisticRegressionScores), sum(logisticRegressionScores)/len(logisticRegressionScores)))
