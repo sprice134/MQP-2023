a47ac5b6470dc3f2bc724439103197a66d38fc16
Stephen P <stepheneprice1@gmail.com>
2023-01-24 11:49:57 -0800

    Computed Surgical Data on SuperComputer

---
+++ Code/Wole-Paper1/finalVersions/imbalance_batch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH --job-name="AugImbalance"
+#SBATCH --output="AugImbalance%j.%N.out"
+#SBATCH --partition=gpu-shared
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus=1
+#SBATCH --mem=250g
+#SBATCH --account=wpi102
+#SBATCH -t 08:00:00
+
+python augmentationImbalanceF1CrossVal_Modular.py
